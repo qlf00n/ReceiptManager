856732c041d71dd12afeced9f46e4921d698fdbe
qlf00n <qlf00n@gmail.com>
2013-02-17 09:16:03 +0100

    Minor modifications to the form, removed empty test class.

--- ReceiptManager/MainForm.Designer.cs
+++ ReceiptManager/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReceiptManager
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Name = "Form1";
+            this.Text = "Receipt Manager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- ReceiptManager/Program.cs
+++ ReceiptManager/Program.cs
@@ -16,7 +16,7 @@ namespace ReceiptManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
